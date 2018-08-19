38d62505a6bd3df5aca2ebd846ea35788c13e685
Basant Besra <besrabasant@gmail.com>
2018-08-19 20:29:11 +0530

    update webpack mix

--- web/app/themes/yellowstudio/webpack.mix.js
+++ web/app/themes/yellowstudio/webpack.mix.js
@@ -1,7 +1,6 @@
 let mix = require('laravel-mix');
-let purgeCss = require('purgecss-webpack-plugin');
-let glob = require('glob-all');
-let path = require('path');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
 
 /*
  |--------------------------------------------------------------------------
@@ -14,53 +13,60 @@ let path = require('path');
  |
  */
 
- if (mix.inProduction()) {
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+if (mix.inProduction()) {
   mix.webpackConfig({
     plugins: [
-    new purgeCss({
-      paths: glob.sync([
-        path.join(__dirname, 'resources/views/**/*.blade.php'),
-        path.join(__dirname, 'resources/assets/js/**/*.vue')
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "resources/views/**/*.blade.php"),
+          path.join(__dirname, "resources/assets/js/**/*.vue")
         ]),
-      extractors: [
-      {
-        extractor: class {
-          static extract(content) {
-            return content.match(/[A-z0-9-:\/]+/g)
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["html", "js", "php", "vue"]
           }
-        },
-        extensions: ['html', 'js', 'php', 'vue']
-      }
-      ]
-    })
+        ]
+      })
     ]
-  })
+  });
 }
 
 mix.setPublicPath('dist')
 .js('resources/assets/js/app.js', 'js/')
 .js('resources/assets/js/customizer.js', 'js/')
 .extract([
- 'babel-polyfill',
- 'vue'
- ])
+   'babel-polyfill',
+   'vue'
+   ])
 .sass('resources/assets/sass/app.scss', 'css/')
 .options({
- processCssUrls: false,
- postCss: [ require('tailwindcss')('./tailwind.js') ],
+   processCssUrls: false,
+   postCss: [ require('tailwindcss')('./tailwind.js') ],
 })
 
 
 if (!mix.inProduction()) {
- mix.browserSync({
-  proxy: 'yellowstudio.rogue',
-  port: 8080,
-  files: [
-  "resources/assets/sass/**/*.scss", 
-  "resources/assets/js/**/*.js", 
-  "resources/assets/js/**/*.vue", 
-  "resources/views/**/*.blade.php", 
-  ]
+   mix.browserSync({
+    proxy: 'yellowstudio.rogue',
+    port: 8080,
+    files: [
+    "resources/assets/sass/**/*.scss", 
+    "resources/assets/js/**/*.js", 
+    "resources/assets/js/**/*.vue", 
+    "resources/views/**/*.blade.php", 
+    ]
 })
 }
 
